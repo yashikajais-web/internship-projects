708bb3bb275e75e9d983e1cc4f2ca61640b6564e
yashikajais-web <yashikajaiswal097@gmail.com>
2025-07-20 02:23:19 +0530

    Add files via upload

---
+++ music player/data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: "Ishq Mein",
+        path: "1song.mp3",
+        artist:"Sachin jigar",
+        image:"song img.jpg"
+    },
+    {
+        name:"Chor Bazari Phir Se",
+        path:"2song.mp3",
+        artist:"Pritam",
+        cover:"img.jpg"
+    },
+    {
+        name:"Ainvayi ainvayi",
+        path:"3song.mp3",
+        artist:"Salim-Sulaiman",
+        cover:"1.avif"
+    }
+];
